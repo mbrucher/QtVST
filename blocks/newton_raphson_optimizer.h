b2345b888dfded9b9c561852eee6395fbf9384d0
Matthieu Brucher <matthieu.brucher@gmail.com>
2011-02-27 22:52:43 +0100

    Function for the optimization

--- blocks/newton_raphson_optimizer.h
+++ blocks/newton_raphson_optimizer.h
@@ -12,17 +12,27 @@ template<class Function>
 class NewtonRaphsonOptimizer
 {
   Function& function;
+  
+  typedef typename Function::DataType DataType;
+  
+  DataType x0, y0;
+  
+  DataType optimize(DataType x1)
+  {
+    return x1;
+  }
+  
 public:
   NewtonRaphsonOptimizer(Function& function)
-  :function(function)
+  :function(function), x0(0), y0(0)
   {
   }
 
-  void process(const typename Function::DataType* in, typename Function::DataType* out, long size)
+  void process(const DataType* in, DataType* out, long size)
   {
     for(long i = 0; i < size; ++i)
 	{
-	  out[i] = in[i];
+	  out[i] = optimize(in[i]);
 	}
   }
 
