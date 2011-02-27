a713cf7602eda628f7128866ca7cf32a5a22e8db
Matthieu Brucher <matthieu.brucher@gmail.com>
2011-02-27 22:07:55 +0100

    Overdrive optimizer

---
+++ blocks/newton_raphson_optimizer.h
@@ -0,0 +1,22 @@
+/**
+ * \file newton_raphson_optimizer
+ */
+
+#ifndef NEWTONRAPHSONOPTMIZER
+#define NEWTONRAPHSONOPTMIZER
+
+namespace DSP
+{
+
+template<class Function>
+class NewtonRaphsonOptimizer
+{
+
+public:
+
+
+};
+
+}
+
+#endif
