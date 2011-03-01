660dde273b3bc103300c405ffd1fef5515c9fc07
Matthieu Brucher <matthieu.brucher@gmail.com>
2011-03-01 21:48:56 +0100

    Finished the overdrive optimizer

--- blocks/newton_raphson_optimizer.h
+++ blocks/newton_raphson_optimizer.h
@@ -5,6 +5,9 @@
 #ifndef NEWTONRAPHSONOPTMIZER
 #define NEWTONRAPHSONOPTMIZER
 
+#include <iostream>
+#include <limits>
+
 namespace DSP
 {
 
@@ -19,7 +22,23 @@ class NewtonRaphsonOptimizer
   
   DataType optimize(DataType x1)
   {
-    return x1;
+    DataType y1 = y0;
+	
+	for(int i = 0; i < 1000; ++i)
+	{
+	  DataType prime = function.prime(x0, x1, y0, y1);
+	  if(std::abs(prime) < std::numeric_limits<DataType>::epsilon() )
+	  {
+	    return y1;
+	  }
+      DataType yk = y1 - function(x0, x1, y0, y1) / function.prime(x0, x1, y0, y1);
+	  if( std::abs(yk - y1) < 0.00001 )
+	  {
+	    return yk;
+	  }
+	  y1 = yk;
+	}
+    return y1;
   }
   
 public:
@@ -33,6 +52,19 @@ public:
     for(long i = 0; i < size; ++i)
 	{
 	  out[i] = optimize(in[i]);
+	  if(out[i] < 1.)
+	  {
+	    if(!(out[i] > -1))
+		{
+		  out[i] = -1;
+		}
+	  }
+	  else
+	  {
+	    out[i] = 1;
+	  }
+	  x0 = in[i];
+	  y0 = out[i];
 	}
   }
 
