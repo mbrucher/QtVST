5fa42619188588fd95aad82db9d5b86ad8a1608a
Matthieu Brucher <matthieu.brucher@gmail.com>
2011-05-01 23:29:47 +0200

    Basic low and high shelving test. Matplotlib spectrogram is not very helpful :|

--- blocks/main_simple_eq.cpp
+++ blocks/main_simple_eq.cpp
@@ -14,15 +14,25 @@ const unsigned long size = 2000000;
 const unsigned int sample_rate = 96000;
 const unsigned int max_frequency = 20000;
 double in[size];
+double temp[size];
 double out[size];
 
 DSP::MonoFilter<double>* create_low_shelving()
 {
-  DSP::SecondOrderShelvingFilter<DSP::HighPassShelvingCoefficients<double> >* filter = new DSP::SecondOrderShelvingFilter<DSP::HighPassShelvingCoefficients<double> >;
-//  DSP::HighPassShelvingFilter<double >* filter = new DSP::HighPassShelvingFilter<double>;
+  DSP::SecondOrderShelvingFilter<DSP::LowPassShelvingCoefficients<double> >* filter = new DSP::SecondOrderShelvingFilter<DSP::LowPassShelvingCoefficients<double> >;
   filter->set_sampling_frequency(sample_rate);
   filter->set_cut_frequency(100);
-  filter->set_gain(.1);
+  filter->set_gain(.01);
+
+  return filter;
+}
+
+DSP::MonoFilter<double>* create_high_shelving()
+{
+  DSP::SecondOrderShelvingFilter<DSP::HighPassShelvingCoefficients<double> >* filter = new DSP::SecondOrderShelvingFilter<DSP::HighPassShelvingCoefficients<double> >;
+  filter->set_sampling_frequency(sample_rate);
+  filter->set_cut_frequency(15000);
+  filter->set_gain(.01);
 
   return filter;
 }
@@ -30,6 +40,7 @@ DSP::MonoFilter<double>* create_low_shelving()
 int main(int argc, char** argv)
 {
   boost::scoped_ptr<DSP::MonoFilter<double> > low_shelving_filter(create_low_shelving());
+  boost::scoped_ptr<DSP::MonoFilter<double> > high_shelving_filter(create_high_shelving());
 
   for(int i = 0; i < size; ++i)
   {
@@ -37,7 +48,8 @@ int main(int argc, char** argv)
     in[i] = std::sin(boost::math::constants::pi<double>() * ((j + .1) * sample_rate * max_frequency / size) * j);
   }
 
-  low_shelving_filter->process(in, out, size);
+  low_shelving_filter->process(in, temp, size);
+  high_shelving_filter->process(temp, out, size);
 
   std::ofstream infile("in_eq.raw", std::ofstream::binary);
   infile.write(reinterpret_cast<const char*>(in), size * sizeof(double));
