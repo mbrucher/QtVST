56fa7d4e0c80a611ab039fa81377d674e94a65fe
Matthieu Brucher <matthieu.brucher@gmail.com>
2013-03-26 21:48:12 +0000

    Added mind map for future work

---
+++ QtVST.mm
@@ -0,0 +1,101 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="QtVST" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1364334224625"><hook NAME="MapStyle">
+    <conditional_styles>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" LAST="false">
+            <node_level_condition VALUE="1" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" LAST="false">
+            <node_level_condition VALUE="2" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" LAST="false">
+            <node_level_condition VALUE="3" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+        <conditional_style ACTIVE="true" LOCALIZED_STYLE_REF="AutomaticLayout.level,4" LAST="false">
+            <node_level_condition VALUE="4" MATCH_CASE="false" MATCH_APPROXIMATELY="false" COMPARATION_RESULT="0" SUCCEED="true"/>
+        </conditional_style>
+    </conditional_styles>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#18898b">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#cc3300">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#669900">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<node TEXT="VST2" POSITION="right" ID="ID_1596159763" CREATED="1364334226326" MODIFIED="1364334304802">
+<edge COLOR="#7c007c"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="64bits" ID="ID_747617500" CREATED="1364334231365" MODIFIED="1364334237865"/>
+<node TEXT="Integration of QWinWidget" ID="ID_1155922739" CREATED="1364334238741" MODIFIED="1364334247752"/>
+</node>
+<node TEXT="AU" POSITION="right" ID="ID_1845658635" CREATED="1364334249524" MODIFIED="1364334307242">
+<edge COLOR="#007c7c"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Bundle for Effect" ID="ID_1335358739" CREATED="1364334312695" MODIFIED="1364334318955"/>
+<node TEXT="Bundle for UI" ID="ID_1576232322" CREATED="1364334319735" MODIFIED="1364334325323"/>
+</node>
+<node TEXT="SimpleEQ" POSITION="left" ID="ID_301078899" CREATED="1364334258899" MODIFIED="1364334301961">
+<edge COLOR="#7c7c00"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Generic effect" ID="ID_1772336056" CREATED="1364334352535" MODIFIED="1364334366963"/>
+<node TEXT="VST 64 plugin" ID="ID_926270880" CREATED="1364334367695" MODIFIED="1364334372555"/>
+<node TEXT="AU 64 plugin" ID="ID_809901769" CREATED="1364334373359" MODIFIED="1364334376874"/>
+</node>
+<node TEXT="SimpleOverdrive" POSITION="left" ID="ID_1357145282" CREATED="1364334264420" MODIFIED="1364334300057">
+<edge COLOR="#ff0000"/>
+<cloud COLOR="#f0f0f0" SHAPE="ARC"/>
+<node TEXT="Generic effect" ID="ID_1381131179" CREATED="1364334379439" MODIFIED="1364334385219"/>
+<node TEXT="VST 64 plugin" ID="ID_1960335457" CREATED="1364334386615" MODIFIED="1364334389898"/>
+<node TEXT="AU 64 plugin" ID="ID_387325440" CREATED="1364334390583" MODIFIED="1364334395402"/>
+</node>
+<node TEXT="CompressorEQ" POSITION="right" ID="ID_1305105082" CREATED="1364334442018" MODIFIED="1364334448997">
+<edge COLOR="#00ff00"/>
+<node TEXT="Generic effect" ID="ID_102947222" CREATED="1364334450146" MODIFIED="1364334453461"/>
+<node TEXT="VST 64 plugin" ID="ID_1386695038" CREATED="1364334454426" MODIFIED="1364334459069"/>
+<node TEXT="AU 64 plugin" ID="ID_1107890332" CREATED="1364334459497" MODIFIED="1364334463765"/>
+</node>
+</node>
+</map>
