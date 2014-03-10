8a69c2ac7d6037fe33c191379b6e627e587ece27
Matthieu Brucher <matthieu.brucher@gmail.com>
2014-03-10 12:17:32 +0000

    Small changes, small differences (1e-9), but good speedup in Python, even though the number of iterations stays constant.

--- blocks/simple_overdrive.py
+++ blocks/simple_overdrive.py
@@ -2,8 +2,6 @@
 
 from __future__ import division
 
-import numpy as np
-
 comp = 0
 
 class Function(object):
@@ -22,7 +20,26 @@ class Function(object):
 
   def gradient(self, x0, x1, y0, y1):
     return self.fprime(y1) + 1/self.A
-    
+
+class Function2(object):
+  def __init__(self, dt, R, C):
+    self.A = dt / (2 * C) + R
+    self.B = dt / (2 * C) - R
+
+  def f(self, x):
+    e = np.exp(x / 26e-3)
+    return 1e-12 * (e - 1/e)
+
+  def fprime(self, x):
+    e = np.exp(x / 26e-3)
+    return 1e-12 * (e + 1/e) / 26e-3
+
+  def __call__(self, x0, x1, y0, y1):
+    return self.f(y1) + 1/self.A * (y1 + (x0 - x1 + self.B * self.f(y0) - y0))
+
+  def gradient(self, x0, x1, y0, y1):
+    return self.fprime(y1) + 1/self.A
+ 
 def optimize(fun, x0, x1, y0):
   global comp
   y1 = y0
@@ -37,7 +54,7 @@ def optimize(fun, x0, x1, y0):
   return y1
 
 def plot_fft(x, fs):
-  n = len(x)
+  import numpy as np
   x = x[-fs:] / (fs / 2)
   x_fft = fft.fft(x)[:fs/2]
   plt.subplot(211)
@@ -48,7 +65,24 @@ def plot_fft(x, fs):
   plt.plot(angle)
 
 def overdrive(signal, fs):
+  import numpy as np
   fun = Function(1./fs, 10000, 22e-9)
+  y = np.zeros(signal.shape)
+  x1 = signal[0]
+  y1 = y[0]
+  
+  for i in range(1, signal.shape[0]):
+    x0 = x1
+    x1 = signal[i]
+    y0 = y1
+    y1 = optimize(fun, x0, x1, y0)
+    y[i] = y1
+
+  return y  
+
+def overdrive2(signal, fs):
+  import numpy as np
+  fun = Function2(1./fs, 10000, 22e-9)
   y = np.zeros(signal.shape)
   x1 = signal[0]
   y1 = y[0]
@@ -76,16 +110,25 @@ if __name__ == "__main__":
   x = 20 * np.sin(2 * np.pi * np.arange(n_samples)*fsin/fs)
   y = overdrive(x, fs = fs)
   print "Mean iterations: ", comp/n_samples
+  comp1 = comp
+  y2 = overdrive2(x, fs = fs)
+  print "Mean iterations: ", (comp-comp1)/n_samples
+
 
   plt.plot(x)
   plt.plot(y)
+  plt.plot(y2)
 
   plt.figure()
   plot_fft(x, fs)
 
   plt.figure()
   plot_fft(y, fs)
-  
+  plt.figure()
+  plot_fft(y2, fs)
+  plt.figure()
+  plt.plot(y2 - y)
+
   wav.write("sin.wav", fs, (x / 20 * 2**15).astype(np.int16))
   wav.write("sin_overdrive.wav", fs, (y * 2**15).astype(np.int16))
   
